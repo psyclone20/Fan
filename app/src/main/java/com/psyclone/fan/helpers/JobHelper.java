b73e7cf43f7fbde0fcfada737b08d911ca1d175e
psyclone20 <adnan.ans26@gmail.com>
2017-12-05 05:05:59 +0530

    Reduce duration between refreshes from 12h to 6h

--- app/src/main/java/com/psyclone/fan/helpers/JobHelper.java
+++ app/src/main/java/com/psyclone/fan/helpers/JobHelper.java
@@ -13,11 +13,12 @@ import static android.app.job.JobInfo.NETWORK_TYPE_ANY;
 
 public class JobHelper {
     private static int JOB_ID = 2810;
+    private static int HOURS_BETWEEN_REFRESH = 6;
 
     public static void scheduleJob(Context mContext) {
         JobScheduler jobScheduler = (JobScheduler) mContext.getSystemService(Context.JOB_SCHEDULER_SERVICE);
         jobScheduler.schedule(new JobInfo.Builder(JOB_ID, new ComponentName(mContext, TVGuideRefreshJob.class))
-                .setPeriodic(TimeUnit.HOURS.toMillis(12))
+                .setPeriodic(TimeUnit.HOURS.toMillis(HOURS_BETWEEN_REFRESH))
                 .setPersisted(true)
                 .setRequiredNetworkType(NETWORK_TYPE_ANY)
                 .build());
